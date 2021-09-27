6df6800eb8ac68153ebd15febcf14a7c7ebb7b2d
Justin Delille <justind7513@gmail.com>
2021-09-27 13:58:06 -0700

    hhhh

--- project.js
+++ project.js
@@ -2,4 +2,8 @@ function getGreeting(name){
     return "Hi " + name;
 }
 
+function getGreeting(color) {
+    return "The color is " + color
+}
+
 module.exports = getGreeting;
--- test.js
+++ test.js
@@ -3,3 +3,7 @@ const getGreeting = require('./project');
 test('can create a basic greeting', () => {
   expect(getGreeting('Sam')).toBe("Hi Sam");
 });
+
+test('testing if color is blue', () => {
+  expect(getGreeting('Blue')).toBe("The color is Blue");
+});
